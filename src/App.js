74095d74a693f9a731144e04427c18972f866afc
rohitkrishna094 <rohitkrishna094@gmail.com>
2019-08-31 04:30:06 -0400

    changed to class component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React, { Component } from 'react';
 import Tone from 'tone';
 import { mapping } from './keymapping';
 import './App.css';
 
-function App() {
-  const [keyMap, setKeyMap] = useState({});
+const pressedKeys = keyMap => {
+  let res = [];
+  for (let key in keyMap) {
+    if (keyMap[key]) res.push(key);
+  }
+  return res;
+};
 
-  useEffect(() => {
-    document.addEventListener('keydown', onKeyDown);
-    document.addEventListener('keyup', onKeyUp);
-  }, [keyMap]);
+class App extends Component {
+  componentDidMount() {
+    document.addEventListener('keydown', this.onKeyDown);
+    document.addEventListener('keyup', this.onKeyUp);
+  }
 
-  const onKeyDown = e => {
-    e.preventDefault();
-    setKeyMap(prevKeyMap => ({ ...prevKeyMap, [e.key]: true }));
+  state = { keyMap: {} };
+
+  onKeyDown = e => {
+    // e.preventDefault();
+    this.setState({ ...this.state, keyMap: { ...this.state.keyMap, [e.key]: true } });
   };
-  const onKeyUp = e => {
-    e.preventDefault();
-    setKeyMap(prevKeyMap => ({ ...prevKeyMap, [e.key]: false }));
+  onKeyUp = e => {
+    // e.preventDefault();
+    this.setState({ ...this.state, keyMap: { ...this.state.keyMap, [e.key]: false } });
   };
 
-  const pressedKeys = keyMap => {
-    let res = [];
-    for (let key in keyMap) {
-      if (keyMap[key]) res.push(key);
-    }
-    return res;
-  };
-
-  const sing = () => {
+  sing = () => {
     //create a synth and connect it to the master output (your speakers)
     const synth = new Tone.Synth().toMaster();
 
@@ -36,11 +36,13 @@ function App() {
     synth.triggerAttackRelease('C4', '8n');
   };
 
-  return (
-    <div className="App">
-      <h1>Currently Playing {JSON.stringify(pressedKeys(keyMap))}</h1>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <h1>Currently Playing {JSON.stringify(pressedKeys(this.state.keyMap))}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
