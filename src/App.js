06c13ca35965a00986ab7b675e4d458e69a55f90
rohitkrishna094 <rohitkrishna094@gmail.com>
2019-08-31 04:16:30 -0400

    added keydown events

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Tone from 'tone';
+import { mapping } from './keymapping';
 import './App.css';
 
 function App() {
+  const [keyMap, setKeyMap] = useState({});
+
+  useEffect(() => {
+    document.addEventListener('keydown', onKeyDown);
+    document.addEventListener('keyup', onKeyUp);
+  }, [keyMap]);
+
+  const onKeyDown = e => {
+    e.preventDefault();
+    setKeyMap(prevKeyMap => ({ ...prevKeyMap, [e.key]: true }));
+  };
+  const onKeyUp = e => {
+    e.preventDefault();
+    setKeyMap(prevKeyMap => ({ ...prevKeyMap, [e.key]: false }));
+  };
+
+  const pressedKeys = keyMap => {
+    let res = [];
+    for (let key in keyMap) {
+      if (keyMap[key]) res.push(key);
+    }
+    return res;
+  };
+
   const sing = () => {
     //create a synth and connect it to the master output (your speakers)
     const synth = new Tone.Synth().toMaster();
@@ -13,8 +38,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Hello to Toy-DAW</h1>
-      <button onClick={sing}>Click Me</button>
+      <h1>Currently Playing {JSON.stringify(pressedKeys(keyMap))}</h1>
     </div>
   );
 }
