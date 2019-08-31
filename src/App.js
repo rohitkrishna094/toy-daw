fc11e668c3c3186a278921e52b6e47946994233b
rohitkrishna094 <rohitkrishna094@gmail.com>
2019-08-30 23:46:54 -0400

    added tone js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import Tone from 'tone';
 import './App.css';
 
 function App() {
+  const sing = () => {
+    //create a synth and connect it to the master output (your speakers)
+    const synth = new Tone.Synth().toMaster();
+
+    //play a middle 'C' for the duration of an 8th note
+    synth.triggerAttackRelease('C4', '8n');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello to Toy-DAW</h1>
+      <button onClick={sing}>Click Me</button>
     </div>
   );
 }
