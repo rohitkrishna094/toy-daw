3f3f29f40e80c06e2c70918e61f05cbe888cf972
rohitkrishna094 <rohitkrishna094@gmail.com>
2021-06-21 01:33:13 -0400

    Add Draggable package

--- src/components/StepSequencer/StepSequencer.js
+++ src/components/StepSequencer/StepSequencer.js
@@ -6,6 +6,7 @@ import Nexus from 'nexusui';
 import './StepSequencer.scss';
 import Dial from '../core/Dial/Dial';
 import Oscilloscope from '../core/Oscilloscope/Oscilloscope';
+import Draggable from 'react-draggable';
 
 const defaultProps = {
   BPM: 120,
@@ -146,46 +147,48 @@ const StepSequencer = () => {
   };
 
   return (
-    <div className="step-sequencer-container">
-      <webaudio-pianoroll
-        ref={pianoRollRef}
-        editMode="dragpoly"
-        style={{ overflowY: 'scroll' }}
-        xRange={256}
-        yRange={20}
-        width="1000"
-        xScroll={1}
-        yScroll={1}
-        wheelZoom={1}
-        markStart={0}
-        markEnd={256}
-        collt="#34444E"
-        coldk="#2E3E48"
-        colgrid="#24343E"
-        colnote="#A4D4AD"
-        colnotesel="#AFD4BA"
-        colnoteborder="#A4D4AD"
-        colrulerbg="#1F2A32"
-        colrulerfg="#b0b0b0"
-        // colrulerborder="pink"
-        kbwidth="80"
-      ></webaudio-pianoroll>
-      <Flex flexDir="column" mt={5}>
-        <h3>{BPM} BPM</h3>
-        <Slider w="500px" defaultValue={defaultProps.BPM} min={defaultProps.minBPM} max={defaultProps.maxBPM} step={defaultProps.BPMStep} onChange={onBPMSliderChange}>
-          <SliderTrack>
-            <SliderFilledTrack />
-          </SliderTrack>
-          <SliderThumb />
-        </Slider>
-        <Oscilloscope uiProps={{ id: 'oscilloscope' }} destination={Tone.Destination} />
-        <Button colorScheme="blue" onClick={onClick} pl={100} pr={100}>
-          {play ? 'Pause' : 'Play'}
-        </Button>
-        <Dial onChange={(v) => setMasterVolume(v)} uiProps={{ id: 'volume', mt: 5 }} />
-        <Flex id="sequencer" mt={5} ref={seqRef}></Flex>
+    <Draggable bounds="parent">
+      <Flex className="step-sequencer-container">
+        <webaudio-pianoroll
+          ref={pianoRollRef}
+          editMode="dragpoly"
+          style={{ overflowY: 'scroll' }}
+          xRange={256}
+          yRange={20}
+          width="1000"
+          xScroll={1}
+          yScroll={1}
+          wheelZoom={1}
+          markStart={0}
+          markEnd={256}
+          collt="#34444E"
+          coldk="#2E3E48"
+          colgrid="#24343E"
+          colnote="#A4D4AD"
+          colnotesel="#AFD4BA"
+          colnoteborder="#A4D4AD"
+          colrulerbg="#1F2A32"
+          colrulerfg="#b0b0b0"
+          // colrulerborder="pink"
+          kbwidth="80"
+        ></webaudio-pianoroll>
+        <Flex flexDir="column" mt={5}>
+          <h3>{BPM} BPM</h3>
+          <Slider w="500px" defaultValue={defaultProps.BPM} min={defaultProps.minBPM} max={defaultProps.maxBPM} step={defaultProps.BPMStep} onChange={onBPMSliderChange}>
+            <SliderTrack>
+              <SliderFilledTrack />
+            </SliderTrack>
+            <SliderThumb />
+          </Slider>
+          <Oscilloscope uiProps={{ id: 'oscilloscope' }} destination={Tone.Destination} />
+          <Button colorScheme="blue" onClick={onClick} pl={100} pr={100}>
+            {play ? 'Pause' : 'Play'}
+          </Button>
+          <Dial onChange={(v) => setMasterVolume(v)} uiProps={{ id: 'volume', mt: 5 }} />
+          <Flex id="sequencer" mt={5} ref={seqRef}></Flex>
+        </Flex>
       </Flex>
-    </div>
+    </Draggable>
   );
 };
 
